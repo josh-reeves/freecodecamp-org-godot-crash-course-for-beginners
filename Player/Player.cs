62cdd786670c09683c4c78b530e21ced6fe36032
Joshua Reeves <>
2023-09-23 13:36:14 -0500

    Added walls, modified player collision box, and began animating player.

--- Player/Player.cs
+++ Player/Player.cs
@@ -1,21 +1,36 @@
 using Godot;
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 public partial class Player : CharacterBody2D
 {
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
 
+
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
-	public override void _PhysicsProcess(double delta)
+	AnimationPlayer playerAnim;
+
+    public override void _Ready()
+    {
+		playerAnim = (AnimationPlayer)GetNode("AnimationPlayer");
+
+    }
+
+    public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
 
 		// Add the gravity.
 		if (!IsOnFloor())
+		{
 			velocity.Y += gravity * (float)delta;
+
+			playerAnim.Play("Fall");
+
+		}
 
 		// Handle Jump.
 		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
@@ -32,6 +47,8 @@ public partial class Player : CharacterBody2D
 		{
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
 		}
+
+		playerAnim.Play("Idle");
 
 		Velocity = velocity;
 		MoveAndSlide();
